409a7fd73fd857481652589df3877eb7b3bd8288
Matthias Schnetzer <mschnetz@wu.ac.at>
2023-02-16 13:13:55 +0100

    Built site for gh-pages

--- data/code/03_r.R
+++ data/code/03_r.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 
-
 ############
 ## PART 1 ##
 ############
 
+# Load library
 library(palmerpenguins)
 
+# Assign data
 data <- penguins
 
 # Take a look at the data
@@ -94,4 +95,75 @@ top10p <- data |> slice_min(bill_length_mm, prop = 0.1)
 ## PART 2 ##
 ############
 
+devtools::install_github("ropengov/eurostat")
 library(eurostat)
+
+# Search eurostat database for GDP data
+search_eurostat("GDP") |> View()
+
+# Download the complete national accounts dataset
+gdp <- get_eurostat("nama_10_gdp", time_format = "num", type = "label")
+
+# Let's take a look at all the components
+unique(gdp$na_item)
+unique(gdp$unit)
+
+gdpat <- gdp |> filter(geo == "Austria", 
+                       unit == "Current prices, million euro",
+                       na_item %in% c("Gross domestic product at market prices",
+                                      "Compensation of employees"))
+
+gdpat_wide <- gdpat |>
+  pivot_wider(names_from = "na_item", values_from = "values")
+
+# Create sub-datasets
+sub1 <- gdpat_wide |> select(geo, time, `Gross domestic product at market prices`)
+sub2 <- gdpat_wide |> select(geo, time, `Compensation of employees`)
+
+# Join dataset
+fulldata <- left_join(sub1, sub2)
+
+# Calculate wage share in % of GDP
+fulldata <- fulldata |> 
+  mutate(wageshare = `Compensation of employees`/`Gross domestic product at market prices`*100)
+
+fulldata |> 
+  ggplot(aes(x = time, y = wageshare)) + 
+  geom_line()
+
+############
+## PART 3 ##
+############
+
+library(lubridate)
+library(scales)
+library(eurostat)
+
+retdata <- get_eurostat("sts_trtu_m", filters = list(indic_bt = "TOVV", s_adj = "CA", unit = "PCH_SM", nace_r2 = "G47", geo = c("AT","DE","FR","ES","IT","PT")), time_format = "date", select_time = "M", type = "code")
+
+plotdat <- retdata |> 
+  mutate(time = ym(time)) |> 
+  slice_max(time, n = 10, by = geo) |> 
+  label_eurostat(dic = "geo", lang = "en") |> 
+  mutate(geo = case_when(str_detect(geo, "Germany") ~ "Germany", TRUE ~ geo))
+
+plotdat |> 
+  ggplot(aes(x = time, y = values)) +
+  geom_segment(aes(y = 0, yend = values, xend = time, 
+                   color = ifelse(values < 0, "green", "red"))) +
+  geom_hline(yintercept = 0) +
+  geom_point(aes(color = ifelse(values < 0, "green", "red"))) + 
+  facet_wrap(~geo) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  scale_x_date(date_labels = "%b %y", date_breaks = "3 months") +
+  theme_minimal() +
+  labs(x = "", y = "",
+       title = "How did retail sales change in the last 10 months?",
+       subtitle = "Percentage change compared to the same period in previous year",
+       caption = "Note: Data is calendar but not seasonally adjusted\n Data: Eurostat. Figure: @matschnetzer") +
+  theme(legend.position = "none",
+        plot.subtitle = element_text(margin = margin(t = 5, b = 10, unit = "pt")),
+        axis.text.y = element_text(size = 8),
+        axis.text.x = element_text(size = 7),
+        panel.spacing.x = unit(2, "lines"),
+        strip.text = element_text(size = 10))
